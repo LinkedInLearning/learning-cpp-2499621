7617736acc57768d5d40f1615764ec0689c408a4
MiGenk <97099580+MiGenk@users.noreply.github.com>
2024-01-09 18:20:04 +0000

    Main <!-- This repository *does not* accept pull requests (PRs). All pull requests will be closed. See CONTRIBUTING.md for further details. -->

---
+++ src/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// using namespace std;
+
+int main()
+{
+  double var_1 = 6.7;
+  double var_2 = 6.6;
+
+  if (var_1 >= var_2)
+  {
+    std::cout << "IF Kondition ist erfolgreich" << std::endl;
+  }
+  else
+  {
+    std::cout << "IF Kondition ist NICHT erfolgreich" << std::endl;
+  }
+
+  return 0;
+}
